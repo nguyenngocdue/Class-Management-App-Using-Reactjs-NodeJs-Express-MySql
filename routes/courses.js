cf1c5a7ecee2557d69fa6d12834cd4a21e02cc38
NguyenNgocDue <duengocnguyen@gmail.com>
2022-02-19 16:55:44 +0700

    first commit app

---
+++ routes/courses.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const slugify = require('slugify');
+const db = require('../config/db.js')
+
+
+router.post("/create", (req, res) => {
+    const {name} = req.body;
+    // console.log(name);
+    // simple Validation
+    if(!name) return res.status(400).json({msg: "Please enter a course name"})
+
+ // sql for user
+ let sqlCheck = `SELECT * from courses WHERE slug = ?`;
+ let sqlInsert = "INSERT INTO courses SET ?";
+ const slug = slugify(name).toLowerCase();
+
+    db.query(sqlCheck, slug, (err, course) => {
+    if (course.length > 0){
+        console.log(course)
+        return res.status(400).json({ msg: "Course Exists" });
+
+    }
+
+    const data = {
+        course_name: name.toLowerCase(),
+        slug: slugify(name).toLowerCase(),
+    };
+
+    db.query(sqlInsert, data, (err, result) => {
+        if (err) {
+            return res.status(401).json({ msg: "Unable to store data" });
+        }
+            return res.status(200).json({ data });
+        });
+    });
+
+});
+
+module.exports = router;
