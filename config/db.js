1c028a951d2b02566d84db9af6f8ffa3c9fd5ebc
NguyenNgocDue <duengocnguyen@gmail.com>
2022-02-28 14:14:31 +0700

    fix error db.js

--- config/db.js
+++ config/db.js
@@ -9,10 +9,10 @@ connectDatabase = () => {
       // database: process.env.DBNAME,
       // user: process.env.DBUSER,
       // password:  process.env.DB_PASS,
-      host: us-cdbr-east-05.cleardb.net,
-      database: heroku_5d1510ed175ce44,
-      user: b7bafd4bb9f161,
-      password:  c12b8433,
+      host: "us-cdbr-east-05.cleardb.net",
+      database: "heroku_5d1510ed175ce44",
+      user: "b7bafd4bb9f161",
+      password:  "c12b8433",
     });
     db.connect(function (err) {
       if (!err) {
